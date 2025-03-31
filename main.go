11caf2da00b2975cbcd49c6b0640757cd82e0d0f
Frankenmint <Frankenmint@users.noreply.github.com>
2025-03-31 07:53:54 -0700

    add main.go

--- main.go
+++ main.go
@@ -1,1 +1,23 @@
-...
+// This is the entry point for the Luxor KVM Manager CLI/API
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"kvm-manager/api"
+	"kvm-manager/db"
+	"kvm-manager/internal"
+)
+
+func main() {
+	db.Init()
+	internal.InitProvisioning()
+
+	http.HandleFunc("/provision", api.ProvisionHandler)
+	http.HandleFunc("/status", api.StatusHandler)
+	http.HandleFunc("/history", api.HistoryHandler)
+
+	log.Println("Luxor KVM Manager API running on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
